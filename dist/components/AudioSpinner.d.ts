b4b9b8481a92181dceb9601b011b498e02a965ca
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2022-12-08 21:21:39 +0300

    fix import error

--- dist/components/AudioSpinner.d.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from 'solid-js';
-import { SpinnerProps } from '../types';
-export declare const AudioSpinner: Component<SpinnerProps>;
--- dist/components/Circles.d.ts
+++
@@ -1,3 +0,0 @@
-import { Component } from 'solid-js';
-import { SpinnerProps } from '../types';
-export declare const Circles: Component<SpinnerProps>;
