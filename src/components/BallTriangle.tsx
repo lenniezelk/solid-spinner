de389a3c164508a81963f967d086522b8cf76294
Leonard Kioi Kinyanjui <lenny.kinyanjui.lk@gmail.com>
2022-12-17 17:01:55 +0300

    fix ball triangle

--- src/components/BallTriangle.tsx
+++ src/components/BallTriangle.tsx
@@ -10,7 +10,7 @@ export const BallTriangle: Component<SpinnerProps> = (
     <svg
       viewBox="0 0 57 57"
       xmlns="http://www.w3.org/2000/svg"
-      fill="currentColor"
+      stroke="currentColor"
       data-test-name="ball-triangle"
       role="img"
       {...mergedProps}
